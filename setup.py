0ed220b0bba6f3c7bad514b1739c63292e5f7e64
revesansparole <revesansparole@gmail.com>
2016-02-02 11:31:53 +0100

    small comsetic estetic changes

--- setup.py
+++ setup.py
@@ -14,23 +14,23 @@ __revision__ = "$Id: $"
 
 import sys
 import os
-
 from setuptools import setup, find_packages
 from openalea.deploy.metainfo import read_metainfo
 
 # Reads the metainfo file
 metadata = read_metainfo('metainfo.ini', verbose=True)
-for key,value in metadata.iteritems():
-    exec("%s = '%s'" % (key, value))
-
+for key, value in metadata.iteritems():
+    exec ("%s = '%s'" % (key, value))
 
 # Packages list, namespace and root directory of packages
 
 pkg_root_dir = 'src'
-pkgs = [ pkg for pkg in find_packages(pkg_root_dir)]
-top_pkgs = [pkg for pkg in pkgs if  len(pkg.split('.')) < 2]
+pkgs = [pkg for pkg in find_packages(pkg_root_dir)]
+top_pkgs = [pkg for pkg in pkgs if len(pkg.split('.')) < 2]
 packages = pkgs
-package_dir = dict( [('',pkg_root_dir)] + [(namespace + "." + pkg, pkg_root_dir + "/" + pkg) for pkg in top_pkgs] )
+package_dir = dict(
+    [('', pkg_root_dir)] + [(namespace + "." + pkg, pkg_root_dir + "/" + pkg)
+                            for pkg in top_pkgs])
 
 # List of top level wralea packages (directories with __wralea__.py)
 # wralea_entry_points = ['%s = %s'%(pkg,namespace + '.' + pkg) for pkg in
@@ -65,8 +65,8 @@ setup(
     package_dir=package_dir,
 
     # Namespace packages creation by deploy
-    #namespace_packages = [namespace],
-    #create_namespaces = False,
+    # namespace_packages = [namespace],
+    # create_namespaces = False,
     zip_safe=False,
 
     # Dependencies
@@ -77,21 +77,19 @@ setup(
     # Eventually include data in your package
     # (flowing is to include all versioned files other than .py)
     include_package_data=True,
-
+    
     # (you can provide an exclusion dictionary named exclude_package_data to remove parasites).
     # alternatively to global inclusion, list the file to include
-    package_data = {'' : ['*.pyd', '*.so', "*.png", "*.cfg"],},
+    package_data={'': ['*.pyd', '*.so', "*.png", "*.cfg"], },
     share_dirs={'share': 'share'},
     # postinstall_scripts = ['',],
 
     # Declare scripts and wralea as entry_points (extensions) of your package
     entry_points={'wralea': ['infraphenogrid = openalea.infraphenogrid_wralea'],
-        #'console_scripts': [
-        #       'fake_script = openalea.fakepackage.amodule:console_script', ],
-        # 'gui_scripts': [
-        #      'fake_gui = openalea.fakepackage.amodule:gui_script',],
-        #	'wralea': wralea_entry_points
-        },
-    )
-
-
+                  # 'console_scripts': [
+                  #       'fake_script = openalea.fakepackage.amodule:console_script', ],
+                  # 'gui_scripts': [
+                  #      'fake_gui = openalea.fakepackage.amodule:gui_script',],
+                  #	'wralea': wralea_entry_points
+                  },
+)
