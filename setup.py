bd472cbd81d866a202ec4ae363719430950701fa
revesansparole <revesansparole@gmail.com>
2016-02-02 11:30:27 +0100

    changed setup.py to include package data i.e. images, sdist still not working, need a manifest

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
 
     # (you can provide an exclusion dictionary named exclude_package_data to remove parasites).
     # alternatively to global inclusion, list the file to include
-    #package_data = {'' : ['*.pyd', '*.so'],},
+    package_data = {'' : ['*.pyd', '*.so', "*.png", "*.cfg"],},
     share_dirs={'share': 'share'},
     # postinstall_scripts = ['',],
 
